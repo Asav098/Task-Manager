0d4a7b5000cde80f87b1478218a2361474765299
Asav098 <167875225+Asav098@users.noreply.github.com>
2026-08-31 17:57:00 +0545

    added route  for login and signup

--- python/app.py
+++ python/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify , request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from datetime import datetime,timedelta
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +13,11 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(80),unique=True,nullable = False)
+    password_hash = db.Column(db.String(200), nullable = True)
+    
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200),nullable = False)
@@ -16,6 +26,48 @@ class Task(db.Model):
 with app.app_context():
     db.create_all()
 
+@app.route('/api/sigup',methods = ['POST'])
+def signup():
+    data = request.get_json();
+    username = data.get('username');
+    password = data.get('password');
+
+    if not username or not password:
+        return jsonify({"error": "Username and Password Required"}), 400
+
+    existing_user = User.query.filter_by(username = username).first()
+
+    if existing_user:
+        return jsonify({"error" : "Üsername Already Exists"}),400
+
+    hashed_password = generate_password_hash(password)
+    new_user = User(username = username, password_hash = hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User Created Successfully", "id":new_user.id})
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback')
+
+@app.route('/api/login',methods=['POST'])
+def login():
+    data= request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    user = User.query.filter_by(username = username).first()
+
+    if not user or not check_password_hash(user.password_hash,password):
+        return jsonify({"error": "Invalid Username or Password"}), 401
+
+    token = jwt.encode({
+    'user_id': user.id,
+    'exp': datetime.utcnow() + timedelta(hours=24)
+    }, SECRET_KEY, algorithm='HS256')
+
+    return jsonify({"token":token, "username":user.username})
 @app.route('/api/tasks',methods=['POST'])
 def create_task():
     data = request.get_json()
